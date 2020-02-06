3f3866c885a43d4a2bf438b5b35591adeb63eea4
Amartya Mondal <hayyoulistentome@gmail.com>
2020-02-06 22:18:21 +0530

    feat: Remove handler

--- app/src/main/java/info/anwesha/iitp/home/MainActivity.java
+++ app/src/main/java/info/anwesha/iitp/home/MainActivity.java
@@ -88,6 +88,8 @@ public class MainActivity extends AppCompatActivity implements EventsRecyclerAda
 
         FirebaseMessaging.getInstance().subscribeToTopic("all");
         FirebaseMessaging.getInstance().subscribeToTopic("dev");
+
+        populateHeaderView();
     }
 
 //    private void handleIntent(Intent appLinkIntent) {
@@ -144,12 +146,6 @@ public class MainActivity extends AppCompatActivity implements EventsRecyclerAda
 //        else navAccount.setTitle("Login/Register");
 
         new Handler().postDelayed(this::updateApp, 1000);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        new Handler().postDelayed(this::populateHeaderView, 200);
     }
 
     private void populateHeaderView() {
